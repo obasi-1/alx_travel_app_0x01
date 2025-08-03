cc67d459305a48ae887597c058d4dd331fcb0b41
Obasi-sam <113913772+obasi-1@users.noreply.github.com>
2025-08-03 18:36:13 +0100

    Create urls.py

---
+++ alx_travel_app/listings/urls.py
@@ -0,0 +1,11 @@
+# listings/urls.py
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet, basename='listing')
+router.register(r'bookings', BookingViewSet, basename='booking')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = router.urls
